375cce8b5a97e6a30e4dc0c11d960a2e14cf05f2
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-01-21 20:45:28 +0200

    feat: add crypto websocket service with binance stream

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  wsUrl: 'wss://stream.binance.com:9443/ws',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  wsUrl: 'wss://stream.binance.com:9443/ws',
+};
