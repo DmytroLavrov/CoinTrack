d273311b5fccf7862fd4ff5796ee5d2cf8356952
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-01-21 21:01:27 +0200

    feat: connect crypto service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { CryptoService } from './services/crypto.service';
+import { ConnectionStatus, PriceTrend } from '@models/ticker-data.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  protected readonly title = signal('coin-track');
+  public cryptoService: CryptoService = inject(CryptoService);
+
+  public statusClass = computed(() => {
+    const status = this.cryptoService.connectionStatus();
+    return `status-${status}`;
+  });
+
+  public statusDotClass = computed(() => {
+    const status = this.cryptoService.connectionStatus();
+    return status;
+  });
+
+  public statusText = computed(() => {
+    const status = this.cryptoService.connectionStatus();
+    const textMap = {
+      [ConnectionStatus.CONNECTING]: 'Connecting to Binance...',
+      [ConnectionStatus.CONNECTED]: 'Live',
+      [ConnectionStatus.DISCONNECTED]: 'Disconnected',
+      [ConnectionStatus.RECONNECTING]: 'Reconnecting...',
+      [ConnectionStatus.ERROR]: 'Connection Error',
+    };
+    return textMap[status];
+  });
+
+  public isTrendUp = computed(() => this.cryptoService.tickerData().trend === PriceTrend.UP);
+  public isTrendDown = computed(() => this.cryptoService.tickerData().trend === PriceTrend.DOWN);
+
+  public isFlashUp = computed(() => this.isTrendUp());
+  public isFlashDown = computed(() => this.isTrendDown());
+
+  public formatTime(timestamp: number): string {
+    return new Date(timestamp).toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+  }
 }
